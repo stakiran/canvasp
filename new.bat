c3e2ccc71effbe2ea5796a5d3767cb30cf17ca63
stakiran <stakiran@users.noreply.github.com>
2019-10-03 15:14:00 +0900

    add clone the latest previous image.

--- new.bat
+++ new.bat
@@ -14,17 +14,31 @@ for /F "usebackq" %%i in (`dir /b %TEMPLATE_DIRNAME%\*.jpg ^| sort`) do (
 	set /a cnt=!cnt!+1
 	echo !cnt!: %%i
 )
+echo c: Clone from the latest previous image
 
 :select_retrying
 set /p input_num="Input the number you want to use>>>";
 
 set cnt=0
 set targetfilename=NOT_FOUND
+set copysource_targetdir=%TEMPLATE_DIRNAME%
 for /F "usebackq" %%i in (`dir /b %TEMPLATE_DIRNAME%\*.jpg ^| sort`) do (
 	set /a cnt=!cnt!+1
 	if "%input_num%"=="!cnt!" (
 		set targetfilename=%%i
 		break
+	)
+)
+
+if "%input_num%"=="c" (
+	set cnt_for_break_instantly=0
+	for /F "usebackq" %%i in (`dir /b %DATA_DIRNAME%\*.jpg ^| sort /r`) do (
+		set /a cnt_for_break_instantly=!cnt_for_break_instantly!+1
+		if "!cnt_for_break_instantly!"=="1" (
+			set targetfilename=%%i
+			set copysource_targetdir=%DATA_DIRNAME%
+			break
+		)
 	)
 )
 
@@ -42,7 +56,7 @@ set shorttime=%timebase::=%
 set shorttime=%shorttime:~0,6%
 set shortdatetime=%shortdate%_%shorttime%
 set filename=%shortdatetime%_%yourcomment%.jpg
-copy %TEMPLATE_DIRNAME%\%targetfilename% %DATA_DIRNAME%\%filename%
+copy %copysource_targetdir%\%targetfilename% %DATA_DIRNAME%\%filename%
 start "" "%PAINT_APP_PATH%" "%DATA_DIRNAME%\%filename%"
 
 popd
